250e4291336dee1a30c73ac228f27c06833c599d
toto42 <pierrick.libert@gmail.com>
2019-10-10 16:17:53 +0900

    Update config.ts
    E

--- lib/config.ts
+++ lib/config.ts
@@ -1,9 +1,4 @@
 'use strict';
 
 export default class Config {
-  // PostgreSQL config
-  public static readonly pgDB = process.env.PG_DB || '';
-  public static readonly pgUser = process.env.PG_USER || '';
-  public static readonly pgPass = process.env.PG_PASS || '';
-  public static readonly pgHost = process.env.PG_HOST || '';
 }
