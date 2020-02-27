bd26ab64a206d81a72d83a4be991a634f9ce9a03
Pierrick Libert <pierrick.libert@gmail.com>
2020-02-27 16:08:20 +0900

    Change axios handling error

--- lib/http/http.service.ts
+++ lib/http/http.service.ts
@@ -5,32 +5,49 @@ import * as querystring from 'querystring';
 
 export class HttpService {
 
+  private instance: axios.AxiosInstance;
+
+  constructor() {
+    this.instance = axios.default.create({
+      validateStatus: function (status: number) {
+        return status < 500;
+      }
+    });
+  }
+
   public post(url: string, data: any, config?: any): Promise<any> {
     if (!config) {
-      return axios.default.post(url, querystring.stringify(data));
+      return this.instance.post(url, querystring.stringify(data));
     }
-    return axios.default.post(url, data, config);
+    return this.instance.post(url, data, config);
   }
 
   public put(url: string, data: any, config?: any): Promise<any> {
     if (!config) {
-      return axios.default.put(url, querystring.stringify(data));
+      return this.instance.put(url, querystring.stringify(data));
     }
-    return axios.default.put(url, data, config);
+    return this.instance.put(url, data, config);
   }
 
   public get(url: string, data: any, config?: any): Promise<any> {
     if (!config) {
-      return axios.default.get(url + '?' + querystring.stringify(data));
+      return this.instance.get(url + '?' + querystring.stringify(data));
     }
-    return axios.default.get(url + '?' + querystring.stringify(data), config);
+    return this.instance.get(url + '?' + querystring.stringify(data), config);
   }
 
   public getByPath(url: string, config?: any): Promise<any> {
     if (!config) {
-      return axios.default.get(url);
+      return this.instance.get(url);
     }
-    return axios.default.get(url, config);
+    return this.instance.get(url, config);
+  }
+
+  public delete(url: string, config?: any): Promise<any> {
+    if (!config) {
+      return this.instance.delete(url);
+    }
+    return this.instance.delete(url, config);
   }
 
 }
